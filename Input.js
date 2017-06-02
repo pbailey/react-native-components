ce96f4be2905fda8e90a0064ca4573c9230c1ca5
Peter Bailey <peterbailey@runbox.com>
2017-06-02 10:55:30 -0400

    Updated Input component

--- Input.js
+++ Input.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label }) => {
+const Input = ({ label, value, onChangeText, placeholder }) => {
+    const { inputStyle, labelStyle, containerStyle } = styles;
     return (
-        <View>
-            <Text>{label}</Text>
+        <View style={containerStyle}>
+            <Text style={labelStyle}>{label}</Text>
+            <TextInput
+                autoCorrect={false}
+                placeholder={placeholder}
+                style={inputStyle}
+                value={value}
+                onChangeText={onChangeText}
+                underlineColorAndroid='transparent' 
+            />
         </View>
     );
+};
+
+const styles = {
+    inputStyle: {
+        color: '#000',
+        paddingLeft: 5,
+        paddingRight: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    containerStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
 };
 
 export { Input };
